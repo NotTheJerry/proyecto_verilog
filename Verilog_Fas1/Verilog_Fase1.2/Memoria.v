fdb5b320937c9f1793181ac075c62e2e03655c48
alanguxman <167795856+alanguxman@users.noreply.github.com>
2024-05-01 09:46:49 -0600

    Add files via upload

---
+++ Verilog_Fas1/Verilog_Fase1.2/Memoria.v
@@ -0,0 +1,21 @@
+module Memoria( input [31:0]dato, input [31:0]direccion, input sel, //Ewr
+                output reg [31:0] salida );
+
+reg [31:0] array [63:0];
+
+initial begin
+    $readmemb ("data.txt", array);
+end
+
+always @(dato or direccion or sel) begin
+
+    if(sel) begin  //Escribir 1
+        array[direccion]=dato;
+        salida = 32'd0;
+    end
+    else begin //Leer 0
+        salida=array[direccion];
+    end
+
+end
+endmodule
