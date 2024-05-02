52c7af668cb24b5bb034ea5491cba9468c532540
alanguxman <167795856+alanguxman@users.noreply.github.com>
2024-05-01 23:47:49 -0600

    Add files via upload

---
+++ Verilog_Fas1/Verilog_Fase1.2/tb_UnidadDeControl.v
@@ -0,0 +1,38 @@
+module tb_UnidadDeControl ();
+
+reg [5:0] op;
+wire MemToReg, MemToWrite, RegWrite;
+wire [1:0] AluOp;
+
+UnidadDeControl unidadDeControl1( .op( op ),
+.MemToReg( MemToReg ), .MemToWrite( MemToWrite ), .RegWrite( RegWrite ), .AluOp( AluOp ) );
+
+
+initial begin
+    op = 6'b000000;
+    #100;
+end
+
+endmodule
+
+
+module tb_Multiplexor ();
+
+reg [31:0] a, b;
+reg selector;
+wire [31:0] salida;
+
+Multiplexor multiplexor ( .a( a ), .b( b ), .selector( selector ),
+            .salida(salida) );
+
+initial begin
+
+    a=32'd10; b=32'd20; selector=1'b1;
+    #100;
+
+    a=32'd10; b=32'd20; selector=1'b0;
+    #100;
+
+end
+
+endmodule
